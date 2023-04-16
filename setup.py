9a73c114a1a8713fbdadc4e707406476db409801
The1one1 <lci2020029@iiitl.ac.in>
2023-04-16 23:28:55 +0530

    setup file created

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ import subprocess
 cmd = "gdown 1hB3iK7Zr4QMwTaOUbTf5IieVzyXwdaMk"
 p1 = subprocess.call(cmd, shell=True)
 
+cmd = "gdown 1AuqHXisy-7_008SFTUHCVWjUDVMKwSUN"
+p1 = subprocess.call(cmd, shell=True)
+
+cmd = "gdown 1WHhixj-LDBb5_w8-y0ENK7DUYdnGBvTo"
+p1 = subprocess.call(cmd, shell=True)
+
 cmd = "pip install -r requirements.txt"
 p1 = subprocess.call(cmd, shell=True)
